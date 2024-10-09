298aea73e064ebcca0517af2e066673a27f47dc7
melch-raiol <melchraiol@hotmail.com>
2024-10-09 10:15:59 -0300

    hello C#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Escreva seu nome :");
+string name = Console.ReadLine();
+string lastname = Console.ReadLine();
+Console.WriteLine("Bem vindo sr: {0} {1}", name, lastname);
